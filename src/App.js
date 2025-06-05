ec8a292dc03aeabf183374c65858f825209dfb8f
Stephanie Bertram <steph.a.bertram@gmail.com>
2025-06-05 12:59:33 -0700

    fetch data from fakeDataSet api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/fakeDataSet')
+      .then(res => res.json())
+      .then(data => setAds(data))
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ads</h1>
+      <pre>{JSON.stringify(ads, null, 2)}</pre>
     </div>
-  );
+  )
 }
 
 export default App;
